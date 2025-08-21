3630ee0f27432b5c071d748c531fd1b8928743f0
Ethan Huang <ethanhuang13@icloud.com>
2025-08-21 21:15:45 +0800

    feat: add support for multiple schedule languages and update tests
    新增支援多種語言的排程檔案，包括英文和日文，並更新測試以確保所有會議的 speakerID 能在講者資料中找到。

---
+++ Sources/SessionData/Models/DataLanguage.swift
@@ -0,0 +1,27 @@
+//
+//  DataLanguage.swift
+//  SessionData
+//
+//  Created by ethanhuang on 2025/8/21.
+//
+
+import Foundation
+
+public enum DataLanguage: Sendable, CaseIterable {
+  case traditionalChinese
+  case english
+  case japanese
+}
+
+public extension DataLanguage {
+  var fileNameSuffix: String {
+    switch self {
+    case .traditionalChinese:
+      return ""
+    case .english:
+      return "_en"
+    case .japanese:
+      return "_jp"
+    }
+  }
+}
